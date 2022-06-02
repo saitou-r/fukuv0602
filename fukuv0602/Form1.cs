7b6272d9c80564f61b6099a74f6fe78a3a1507f6
A061-09 <tnk.dat.class@gmail.com>
2022-06-02 13:43:21 +0900

    Initial commit

---
+++ fukuv0602/Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace fukuv0602
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("齋藤麗也");
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            label1.Top -= 2;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            label1.Left -= 2;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            label1.Left += 2;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            label1.Top += 2;
+        }
+    }
+}
